2ef6dbaf3db0f6c2a45b7a774e5e47099bcf4abb
Nithiya03 <nithiyasrirud03@gmail.com>
2022-05-05 17:40:23 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormGroup,FormControl, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive';
+  loginform = new FormGroup({
+    user:new FormControl('',[Validators.required]),
+    password:new FormControl('',[Validators.required]),
+  });
+  loginUser(){
+    console.warn(this.loginform.value);
+  }
+
+  get user(){
+    return this.loginform.get('user');
+  }
+
+  get password(){
+    return this.loginform.get('password');
+  }
 }
